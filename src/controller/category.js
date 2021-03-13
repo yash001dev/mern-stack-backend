a014f15087a7bd87d068feaebe8c00962755a74c
unknown <y.sanghavi001@gmail.com>
2021-03-13 16:23:43 +0530

    Product Of Api Added

--- src/controller/category.js
+++ src/controller/category.js
@@ -1,6 +1,28 @@
 const Category=require('../models/category');
 const slugify=require('slugify');
 
+function createCategories(categories,parentId=null){
+    const categoryList=[];
+    let category;
+    if(parentId==null){
+        category=categories.filter(cat=>cat.parentId==undefined);
+    }else{
+        category=categories.filter(cat=>cat.parentId==parentId);
+    }
+
+    for(let cate of category){
+        categoryList.push({
+            _id:cate._id,
+            name:cat._name,
+            slug:cate.slug,
+            children:createCategories(categories,cate._id)
+        });
+    }
+
+    return categoryList;
+}
+
+
 exports.addCategory=(req,res)=>{
     const categoryObject={
         name:req.body.name,
@@ -26,8 +48,11 @@ exports.getCategories=(req,res)=>{
     .exec((error,categories)=>{
         if(error) return res.status(400).json({error});
         if(categories){
+
+            const categoryList=createCategories(categories);
+
             res.status(200).json({
-                categories
+                categoryList
             })
         }
     })
--- src/index.server.js
+++ src/index.server.js
@@ -7,6 +7,7 @@ const mongoose=require('mongoose');
 const authRoutes=require('./routes/auth');
 const adminRoutes=require('./routes/admin/auth');
 const categoryRoutes=require('./routes/category');
+const productRoutes=require('./routes/product');
 
 //environment variable or you can say constants
 env.config();
@@ -28,6 +29,7 @@ app.use(express.json())
 app.use('/api',authRoutes);
 app.use('/api',adminRoutes);
 app.use('/api',categoryRoutes);
+app.use('/api',productRoutes);
 
 
 app.listen(process.env.PORT,()=>{
